3698254fe37e31888f95b96268703ad69e2347c6
Ivan Bogomazov <ibogomazov@mirantis.com>
2016-04-11 18:17:48 +0300

    WIP: implement build rpm package
    Change-Id: I8f1370eb31e15d71213e5072e46aa6f3b8f3bc64

---
+++ packetary/drivers/base_packaging.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+
+#    Copyright 2016 Mirantis, Inc.
+#
+#    This program is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation; either version 2 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License along
+#    with this program; if not, write to the Free Software Foundation, Inc.,
+#    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+import abc
+import logging
+
+import six
+
+
+@six.add_metaclass(abc.ABCMeta)
+class PackagingDriverBase(object):
+    """The super class for Packaging Drivers.
+
+    For implementing support of new type of packaging:
+    - inherit this class
+    - implement all abstract methods
+    - register implementation in 'packetary.drivers' namespace
+    """
+
+    def __init__(self):
+        self.logger = logging.getLogger(__package__)
+
+    @abc.abstractmethod
+    def build_packages(self, release,  sources):
+        """ Build package from sources
+
+        :param release: release like 'centos-7-x86_64'
+        :param sources: path to sources
+        :return: list of builded packages
+        """
+
+    @abc.abstractmethod
+    def get_chroots(self, path_to_configs):
+        """List all available chroot configurations
+
+        :param path_to_configs: path to chroot config files
+        :return: list of available chroots
+        """
